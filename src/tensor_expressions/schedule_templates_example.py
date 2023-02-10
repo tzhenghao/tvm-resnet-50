8de64166a5a92863db2eb773f60b426d2390079e
Zheng Hao Tan <hi@zhenghaotan.com>
2023-02-10 06:45:36 +0000

    add define_split implementation

--- src/tensor_expressions/schedule_templates_example.py
+++ src/tensor_expressions/schedule_templates_example.py
@@ -63,5 +63,35 @@ def matmul_v1(N, L, M, dtype):
     return s, [A, B, C]
 
 
+@autotvm.template("tutorial/matmul")
+def matmul(N, L, M, dtype):
+    A = te.placeholder((N, L), name="A", dtype=dtype)
+    B = te.placeholder((L, M), name="B", dtype=dtype)
+
+    k = te.reduce_axis((0, L), name="k")
+    C = te.compute(
+        (N, M), lambda i, j: te.sum(A[i, k] * B[k, j], axis=k), name="C"
+    )
+    s = te.create_schedule(C.op)
+
+    # schedule
+    y, x = s[C].op.axis
+    k = s[C].op.reduce_axis[0]
+
+    ##### define space begin #####
+    cfg = autotvm.get_config()
+    cfg.define_split("tile_y", y, num_outputs=2)
+    cfg.define_split("tile_x", x, num_outputs=2)
+    ##### define space end #####
+
+    # schedule according to config
+    yo, yi = cfg["tile_y"].apply(s, C, y)
+    xo, xi = cfg["tile_x"].apply(s, C, x)
+
+    s[C].reorder(yo, xo, k, yi, xi)
+
+    return s, [A, B, C]
+
+
 if __name__ == "__main__":
     pass
