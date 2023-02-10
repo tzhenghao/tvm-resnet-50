c625dca664a065dcc37f7731f6f80c2a99da3b3c
Zheng Hao Tan <hi@zhenghaotan.com>
2023-02-10 05:23:39 +0000

    cleanup

--- src/tensor_expressions/schedule_templates_example.py
+++ src/tensor_expressions/schedule_templates_example.py
@@ -7,3 +7,61 @@ import numpy as np
 import tvm
 import tvm.testing
 from tvm import autotvm, te
+
+
+def matmul_basic(N, L, M, dtype):
+    A = te.placeholder((N, L), name="A", dtype=dtype)
+    B = te.placeholder((L, M), name="B", dtype=dtype)
+
+    k = te.reduce_axis((0, L), name="k")
+    C = te.compute(
+        (N, M), lambda i, j: te.sum(A[i, k] * B[k, j], axis=k), name="C"
+    )
+    s = te.create_schedule(C.op)
+
+    # schedule
+    y, x = s[C].op.axis
+    k = s[C].op.reduce_axis[0]
+
+    yo, yi = s[C].split(y, 8)
+    xo, xi = s[C].split(x, 8)
+
+    s[C].reorder(yo, xo, k, yi, xi)
+
+    return s, [A, B, C]
+
+
+# Matmul V1: List candidate values
+@autotvm.template("tutorial/matmul_v1")  # 1. use a decorator
+def matmul_v1(N, L, M, dtype):
+    A = te.placeholder((N, L), name="A", dtype=dtype)
+    B = te.placeholder((L, M), name="B", dtype=dtype)
+
+    k = te.reduce_axis((0, L), name="k")
+    C = te.compute(
+        (N, M), lambda i, j: te.sum(A[i, k] * B[k, j], axis=k), name="C"
+    )
+    s = te.create_schedule(C.op)
+
+    # schedule
+    y, x = s[C].op.axis
+    k = s[C].op.reduce_axis[0]
+
+    # 2. get the config object
+    cfg = autotvm.get_config()
+
+    # 3. define search space
+    cfg.define_knob("tile_y", [1, 2, 4, 8, 16])
+    cfg.define_knob("tile_x", [1, 2, 4, 8, 16])
+
+    # 4. schedule according to config
+    yo, yi = s[C].split(y, cfg["tile_y"].val)
+    xo, xi = s[C].split(x, cfg["tile_x"].val)
+
+    s[C].reorder(yo, xo, k, yi, xi)
+
+    return s, [A, B, C]
+
+
+if __name__ == "__main__":
+    pass
